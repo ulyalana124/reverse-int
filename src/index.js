1af1bd8378347c7ff21e0387f9d0fbc28edf3ec6
Uliana <ulanasadikova65@gmail.com>
2021-03-07 21:08:55 +0200

    feat: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+   let strNum = '' + n;
+   let arrNum = strNum.match(/\d/gi);
+   let revNum = arrNum.reverse();
+   let newNum = revNum.join([]);
+   return +newNum;
+};
